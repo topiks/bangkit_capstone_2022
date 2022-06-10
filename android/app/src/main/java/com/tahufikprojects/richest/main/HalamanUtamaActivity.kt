ceaf1c48618b42a1ef1ea3256c8366ae0c73d84a
lalala910 <79497964+lalala910@users.noreply.github.com>
2022-06-10 20:09:19 +0700

    Update HalamanUtamaActivity.kt

--- android/app/src/main/java/com/tahufikprojects/richest/main/HalamanUtamaActivity.kt
+++ android/app/src/main/java/com/tahufikprojects/richest/main/HalamanUtamaActivity.kt
@@ -36,4 +36,10 @@ class HalamanUtamaActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+    
+    fun asSyncAdapter(uri: Uri, account: String, accountType: String): Uri {
+        return uri.buildUpon()
+            .appendQueryParameter(CalendarContract.CALLER_IS_SYNCADAPTER, "true")
+            .appendQueryParameter(CalendarContract.Calendars.ACCOUNT_NAME, account)
+            .appendQueryParameter(CalendarContract.Calendars.ACCOUNT_TYPE, accountType).build()
 }
