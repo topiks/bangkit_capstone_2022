0beff6c30e234e26fc4d6e1c31a623d2d520c95d
Anastasia Amanda Harumdiana Tobong <82534276+amandatobong20@users.noreply.github.com>
2022-06-13 00:41:31 +0700

    Delete backend-service directory

--- backend-service/routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const getIndexHandler = require("../handler/index");
-
-router.get("/", getIndexHandler);
-
-module.exports = router;
--- backend-service/utils/localhost.js
+++
@@ -1,4 +0,0 @@
-const domain = process.env.DOMAIN || "backend-richest.et.r.appspot.com";
-const hostname = process.env.NODE_ENV !== "production" ? "localhost": domain;
-
-module.exports = hostname;
