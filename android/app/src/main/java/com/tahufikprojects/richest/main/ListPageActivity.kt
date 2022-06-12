6123c118d63c1bfa9e2f8af4e2f1d8ce4e456500
lalala910 <79497964+lalala910@users.noreply.github.com>
2022-06-12 19:59:24 +0700

    Update and rename list_page.kt to ListPageActivity.kt

--- android/app/src/main/java/com/tahufikprojects/richest/main/ListPageActivity.kt
+++ android/app/src/main/java/com/tahufikprojects/richest/main/ListPageActivity.kt
@@ -6,40 +6,41 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.tahufikprojects.richest.R
 
-class list_page : AppCompatActivity() {
+class ListPageActivity : AppCompatActivity() {
     private lateinit var rvList: RecyclerView
-    private val list = ArrayList<list_data>()
+    private val list = ArrayList<ThingsToDo>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_list_page)
+        setContentView(R.layout.activity_list_page2)
 
         rvList = findViewById(R.id.rv_list)
         rvList.setHasFixedSize(true)
 
         list.addAll(ListData)
         showRecyclerList()
-
     }
 
-
-
-    private val ListData: ArrayList<list_data>
+    private val ListData : ArrayList<ThingsToDo>
     get(){
         val dataDay = resources.getStringArray(R.array.data_day)
         val dataValues = resources.getStringArray(R.array.data_description)
         val dataPhoto = resources.obtainTypedArray(R.array.data_photo)
-        val listDataa = ArrayList<list_data>()
-        for(i in dataDay.indices){
-            val vList = list_data(dataDay[i],dataValues[i],dataPhoto.getResourceId(i, -1))
+        val listDataa = ArrayList<ThingsToDo>()
+        for(i in dataDay.indices) {
+            val vList = ThingsToDo(dataDay[i],dataValues[i],dataPhoto.getResourceId(i, -1))
             listDataa.add(vList)
         }
-        return ListData
+        return listDataa
     }
+
+
 
     private fun showRecyclerList() {
         rvList.layoutManager = LinearLayoutManager(this)
-        val list_DataAdapter = listDataAdapter(list)
-        rvList.adapter = list_DataAdapter
+        val ListDataAdapter = listDataAdapter(list)
+        rvList.adapter = ListDataAdapter
     }
+
+
 }
